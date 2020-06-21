2da4c09c44df9a134f2d7d6cf8fd6f49ab0a112b
Muia23 <wilfred.muema@gmail.com>
2020-06-21 13:54:08 +0300

    import and declare the repos component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
