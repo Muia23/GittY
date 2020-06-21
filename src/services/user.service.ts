517e5a6588c5b5af57387a7a744a6b9ce2cf2197
Muia23 <wilfred.muema@gmail.com>
2020-06-21 13:56:40 +0300

    add user data service

---
+++ src/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private username:string;
+  private apikey:string;
+  
+  constructor(private http:HttpClient) { 
+    console.log("service is ready!");
+    this.username='muia23';
+    this.apikey='aec24ccb4434c857d8600e85ce8ab37794292593';
+  }
+
+  getUserData(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?" + this.apikey +".json");
+  }
+
+  getUserRepo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?" + this.apikey +".json");  
+  }
+}
