654493c990b47c6672499fc929e968c39ddfae22
Muia23 <wilfred.muema@gmail.com>
2020-06-21 18:27:41 +0300

    call the service function to get user repositories

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -7,16 +7,20 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+  user:any;
   repos:any; 
 
   constructor(private userService: UserService) {
+    this.userService.getUserData().subscribe(user => {
+        console.log(user);
+        this.user = user;
+    });
     this.userService.getUserRepo().subscribe(repos => {
-      console.log(repos);
-      this.repos = repos;
+        console.log(repos);
+        this.repos = repos;
     })
-
+  
   }
-
   ngOnInit(): void {
   }
 
