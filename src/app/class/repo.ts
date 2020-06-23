523741ee77d023a77d636957598653a548af3770
Muia23 <wilfred.muema@gmail.com>
2020-06-23 13:14:57 +0300

    add a class folder with user and repo classes

---
+++ src/app/class/repo.ts
@@ -0,0 +1,10 @@
+export class Repo {
+        constructor(
+        public id:number,
+        public name:string,
+        public description:string,
+        public date:Date,
+        public live:string ){
+                
+        }
+}
---
+++ src/app/class/user.ts
@@ -0,0 +1,20 @@
+export class User {
+
+    constructor(
+        public username:string,
+        public avatar:string,
+        public followers:string,
+        public following:string,
+        public name:string,
+        public gists:string,
+        public repos:string,
+        public githublink: string,
+        public bio: string,
+        public email:string,
+        ){
+        
+    }
+
+}
+
+
